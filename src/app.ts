4552d14ff901de2d697aa847c730002cd8bd7527
Julia Lee <juliana.caesaris@gmail.com>
2024-10-28 04:29:38 +0900

    add ejs

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
+import * as ejs from 'ejs';
+
+// console.log(ejs);
+let people: string[] = ['geddy', 'neil', 'alex'];
+ejs.render('<%= people.join(", "); %>', { people }, { async: true }).then((html) => {
+  console.log(html);
+});
+
 console.log('Hello World');
+
